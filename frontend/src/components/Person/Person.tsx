598c7225376ecd8798e233683d0ceaaf584b9ced
Gilles Tanson <gillestanson@googlemail.com>
2022-11-20 09:39:15 +0100

    updated team page

--- frontend/src/components/Person/Person.tsx
+++ frontend/src/components/Person/Person.tsx
@@ -1,103 +1,104 @@
-import type { PersonProps } from "./types"
-import {Button, Card, CardActions, CardContent, CardMedia, Grid, Typography} from "@mui/material";
-export function Person({ prop = "Person" }: PersonProps) {
-return (
-  <div className="w-full h-screen bg-black flex flex-col overflow-hidden">
-    <div style={{width:"60%"}}>
-      <Typography gutterBottom variant="h2" component="div" style={{color: "rgb(255,95, 0)", padding:"2rem"}}>
-        Our Awesome Team
-      </Typography>
-      <Grid container rowSpacing={4} columnSpacing={1}>
-        <Grid item xs={6}>
-          <Card sx={{ maxWidth: 345, marginLeft:"2rem"}}>
-            <CardMedia
-              component="img"
-              height="140"
-              image="src/assets/team/tilly_1.png"
-              alt="tilly"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Ashmi Banerjee
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Full-stack Developer
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card sx={{ maxWidth: 345,}}>
-            <CardMedia
-              component="img"
-              height="140"
-              image="src/assets/team/tilly_1.png"
-              alt="tilly"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Tilly
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Full-stack Developer
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card sx={{ maxWidth: 345,marginLeft:"2rem"}}>
-            <CardMedia
-              component="img"
-              height="140"
-              image="src/assets/team/tilly_1.png"
-              alt="tilly"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Tilly
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Full-stack Developer
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-        <Grid item xs={6}>
-          <Card sx={{ maxWidth: 345, }}>
-            <CardMedia
-              component="img"
-              height="140"
-              image="src/assets/team/tilly_1.png"
-              alt="tilly"
-            />
-            <CardContent>
-              <Typography gutterBottom variant="h5" component="div">
-                Tilly
-              </Typography>
-              <Typography variant="body2" color="text.secondary">
-                Full-stack Developer
-              </Typography>
-            </CardContent>
-            <CardActions>
-              <Button size="small">Share</Button>
-              <Button size="small">Learn More</Button>
-            </CardActions>
-          </Card>
-        </Grid>
-      </Grid>
-    </div>
+import {
+  Button,
+  Card,
+  CardActions,
+  CardContent,
+  CardMedia,
+  Container,
+  Grid,
+  Typography,
+} from "@mui/material"
 
-  </div>
-)}
+import type { PersonProps } from "./types"
+export function Person({ prop = "Person" }: PersonProps) {
+  return (
+    <div className="w-full bg-black flex flex-col overflow-hidden">
+      <div style={{ width: "100%", marginBottom: "50px"}}>
+        <Typography
+          gutterBottom
+          variant="h2"
+          component="div"
+          style={{ color: "rgb(255,95, 0)", padding: "2rem" }}
+        >
+          Our Awesome Team
+        </Typography>
+        <Container>
+          <Grid container rowSpacing={4} columnSpacing={1}>
+            <Grid item xs={6}>
+              <Card sx={{ maxWidth: 345, marginLeft: "2rem" }}>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image="src/assets/team/ashmi.png"
+                  alt="tilly"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Ashmi Banerjee
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Backend Developer
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={6}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image="src/assets/team/tilly_1.png"
+                  alt="tilly"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Tilly
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Full-stack Developer
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={6}>
+              <Card sx={{ maxWidth: 345, marginLeft: "2rem" }}>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image="src/assets/team/Gilles.jpg"
+                  alt="tilly"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Gilles
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Frontend Developer
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+            <Grid item xs={6}>
+              <Card sx={{ maxWidth: 345 }}>
+                <CardMedia
+                  component="img"
+                  height="300"
+                  image="src/assets/team/Mantas.png"
+                  alt="tilly"
+                />
+                <CardContent>
+                  <Typography gutterBottom variant="h5" component="div">
+                    Mantas
+                  </Typography>
+                  <Typography variant="body2" color="text.secondary">
+                    Data Engineer
+                  </Typography>
+                </CardContent>
+              </Card>
+            </Grid>
+          </Grid>
+        </Container>
+      </div>
+    </div>
+  )
+}
